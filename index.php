40671766df38b6e0e79c1f392aa1b3b49ad249fc
Scott Yamamoto <scott.yamamoto@hotmail.com>
2023-01-09 20:59:25 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php namespace MakingFunOfGaara;
+
+?>
+  <!DOCTYPE html>
+  <html>
+    <style>
+      header { display: flex; }
+      header > div { 
+        display: flex; 
+        align-items:center;
+		    flex-direction:column;
+		    width:100%;
+      }
+    </style>
+    <body>
+      <header>
+        <div>
+          <h1>Gaara won't learn coding smh</h1>
+          <p><strong>So I'm gonna make fun of him instead</p></strong>
+        </div>
+      </header>
+
+      <script>
+
+      </script>
+    </body>
+  </html>
+<?php
+  $host = 'localhost';
+  $username = 'id20126114_daes';
+  $password = 'AnotherPassword123!';
